964f5abdf1ab28a3c63eee580c1d0abecde839ab
falachabt <bennytenezeu@gmail.com>
2023-03-21 14:41:05 -0700

    test1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,8 +5,7 @@ int _putchar(char c);
 
 void print_alphabet(void)
 {
-    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k\
-', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     int i = 0;
 
     for( i = 0; i<26; i++)
@@ -14,7 +13,7 @@ void print_alphabet(void)
       _putchar(alphabet[i]);
     }
     _putchar('\n');
-}
+};
 
 
 #endif /* MAIN_H */
