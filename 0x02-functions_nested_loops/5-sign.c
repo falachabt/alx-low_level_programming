a9e4f926067aff930754d16e72e02bd42961743a
falachabt <bennytenezeu@gmail.com>
2023-03-21 16:22:05 -0700

    yes sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a givent number
+ * @n: number to check the sign 
+ *
+ *return: 1 if positif, 0 if zeo, and -1 of negatif
+ */
+int print_sign(int n)
+{
+if(n > 0){
+_putchar('+');
+return (1);
+}else if(n == 0){
+_putchar('0');
+return (0);
+}else{
+_putchar('-');
+return (-1);
+}
+}
