f73e7d0429755b357a1929f5e2ab2b884f4e5c2f
falachabt <bennytenezeu@gmail.com>
2023-03-21 14:31:46 -0700

    update of alphaet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,9 @@
 #include "main.h"
 
-int main(void)
-{
-  print_alphabet();
-  return(0);
-}
-
 void print_alphabet(void)
 {
-    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k\
+', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     int index = 0;
 
     for(int i = 0; i<26; i++)
@@ -16,4 +11,10 @@ void print_alphabet(void)
       _putchar(alphabet[i]);
     }
     _putchar('\n');
+}
+
+int main(void)
+{
+  print_alphabet();
+  return(0);
 }
