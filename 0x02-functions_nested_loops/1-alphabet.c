e9989d2d9ada11a21cc6ffa34c44db5f3b2d8e44
falachabt <bennytenezeu@gmail.com>
2023-03-21 15:19:04 -0700

    yesssss

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,14 @@
  *this function return all letters of alphabet in the lower case
  *
  */
-
 void print_alphabet(void)
 {
     char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
-                          'j','k','l','m','n','o','p','q','r','s',
-			  't','u','v','w','x','y','z'};
+                          'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's',
+			  't', 'u', 'v', 'w', 'x', 'y', 'z'};
     int i;
-    for( i = 0; i<26; i++)
+    
+    for(i = 0; i<26; i++)
     {
       _putchar(alphabet[i]);
     }
