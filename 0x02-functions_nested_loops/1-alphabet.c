8f73cc9b16c8aa53044cd17eb8a28e9f17275bec
falachabt <bennytenezeu@gmail.com>
2023-03-21 14:36:29 -0700

    udate alphabet 1.0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,5 @@
 #include "main.h"
 
-void print_alphabet(void)
-{
-    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k\
-', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int index = 0;
-
-    for(int i = 0; i<26; i++)
-    {
-      _putchar(alphabet[i]);
-    }
-    _putchar('\n');
-}
-
 int main(void)
 {
   print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,18 @@
 
 int _putchar(char c);
 
+void print_alphabet(void)
+{
+    char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k\
+', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int i = 0;
+
+    for( i = 0; i<26; i++)
+    {
+      _putchar(alphabet[i]);
+    }
+    _putchar('\n');
+}
+
+
 #endif /* MAIN_H */
