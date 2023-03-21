73017e4a48585f8753f092fbbe5d435b4390ac2c
falachabt <bennytenezeu@gmail.com>
2023-03-21 16:25:34 -0700

    test betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,17 +4,20 @@
  * print_sign - print the sign of a givent number
  * @n: number to check the sign 
  *
- *return: 1 if positif, 0 if zeo, and -1 of negatif
+ *Return: 1 if positif, 0 if zeo, and -1 of negatif
  */
 int print_sign(int n)
 {
-if(n > 0){
+if(n > 0)
+  {
 _putchar('+');
 return (1);
-}else if(n == 0){
+}else if(n == 0)
+  {
 _putchar('0');
 return (0);
-}else{
+}else
+  {
 _putchar('-');
 return (-1);
 }
