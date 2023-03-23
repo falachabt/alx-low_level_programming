b66951dca9f52681c3438676bdd1384f14ff7aeb
falachabt <bennytenezeu@gmail.com>
2023-03-23 08:50:41 -0700

    yes 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
 /**
- *print_diagol: draw a diagonal using symbols
+ *print_diagol -  draw a diagonal using symbols
  *@n: the lenght of the diagonal
  *
  *Return: nothing to return
  */
 void print_diagonal(int n)
 {
-  
-if(n>0)
-{
-  
-int i;
- 
-for(i=0; i<n; i++)
-{
+	if (n > 0)
+	{
+		int i;
 
-int j;
-j = 0;
+		for (i = 0; i < n; i++)
+		{
+			int j;
 
-while (j < i)
-{
+			j = 0;
 
-_putchar(' ');
- j++;
-}
-
-_putchar('_');
-_putchar('\n');
-}
-}
+			while (j < i)
+			{
+				_putchar(' ');
+ 				j++;
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
 }
