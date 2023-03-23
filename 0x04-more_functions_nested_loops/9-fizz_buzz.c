c3b159568ee4640eb8a713c3822f7cbbfbfff8e2
falachabt <bennytenezeu@gmail.com>
2023-03-23 11:14:02 -0700

    test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,26 @@ int  main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
+			printf(" ");
 		}
 	}
-	printf("\n");
+	printf("\n ");
 
 	return (0);
 }
