5c395aafbe75e840da5004e2dd31ed070d8708f1
falachabt <bennytenezeu@gmail.com>
2023-03-23 09:12:16 -0700

    square is good

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,36 +1,31 @@
 #include "main.h"
 
 /**
- *print_square: print a square of a pricise size
+ *print_square - print a square of a pricise size
  *@n: the size of the square
  *
  *Return: nothin to return
  */
 void print_square(int n)
 {
+	if (n > 0)
+	{
+		int i;
 
-if(n>0)
-{
+		for (i = 0; i < n; i++)
+		{
+			int j;
 
-int i ;
- 
-for(i=0; i<n; <i++)
-{
+			for (j = 0; j < n; j++)
+			{
 
-int j;
- 
-for (j=0; j<n; j++)
-{
-
-_putchar('#');
-}
-
-_putchar('\n');
-}
-}
-else
-{
-
-_putchar('\n');
-}
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
