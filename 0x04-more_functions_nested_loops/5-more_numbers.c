cce5eb7f5389fee15c7228b16098d69c371eb56b
falachabt <bennytenezeu@gmail.com>
2023-03-23 08:25:54 -0700

    yes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- *print_numbers: return number for 0 to 9
+ *print_numbers -  return number for 0 to 9
  *
  *Return: no return for this function 
  */
 void more_numbers(void)
 {
-int i;
-int j;
+	int i;
+	int j;
 
-for(j = 0; j<10; j++)
-{
+	for(j = 0; j<10; j++)
+	{
+		for(i = 0; i < 14; i++)
+		{
+			_putchar(i);
+		}
 
-for(i = 0; i < 14; i++)
-{
-_putchar(i);
-}
- 
-_putchar('\n');
-}
-
-_putchar('\n');
+		_putchar('\n');
+	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,12 @@ void print_line(int n)
 {
 	if (n > 0)
 	{
-	int i;
+		int i;
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-	}
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
