d1bacf47af405b06de8405396cdb38f8588bb328
falachabt <bennytenezeu@gmail.com>
2023-03-22 22:17:15 -0700

    new canges

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,12 +2,14 @@
 #define MAIN_H
 
 
-//print a character
+/* print a character */
 int _putchar(char c);
 
-// check if c is uppercase
+/*  check if c is uppercase */
 int _isupper(int c);
 
+/* check if c is digit */
+int _isdigit(int c);
 
 
-#endif // MIAN_H
+#endif /*  MIAN_H */
