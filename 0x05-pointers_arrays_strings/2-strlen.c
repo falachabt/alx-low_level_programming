6db2d1c84a78f08276075cee7d1660d343b3d2dd
falachabt <bennytenezeu@gmail.com>
2023-03-27 20:43:04 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - take an word  en return his lenght
+ *@s: the word to get the legnt
+ *
+ * Return: the lenght of the word s
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
