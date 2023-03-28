8d1438bdbf14644957829f106c3de9ddace4dbcf
falachabt <bennytenezeu@gmail.com>
2023-03-28 12:54:59 -0700

    yes

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the value of one pointer to another
+ * @dest: the first pointer
+ * @src: the second pointer
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
