860fdd8f4bb103e8123d78f451fe484fc21fc3c4
falachabt <bennytenezeu@gmail.com>
2023-03-27 20:58:45 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - this function print a integer
+ * @s: the integer to print
+ *
+ * Return: the funciton return nothing
+ */
+
+void _puts(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i=0; i<len; i++)
+	{
+		_putchar(*s[i]);
+	}
+
+	_putchar('\n');
+}
