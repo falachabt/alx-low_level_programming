350240745149cd16b02022530437c5e6a9b29510
falachabt <bennytenezeu@gmail.com>
2023-03-28 08:12:37 -0700

    putst2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print every oddd number
+ * @str: the string from which we are going to print
+ *
+ * Return: nothing to return
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
