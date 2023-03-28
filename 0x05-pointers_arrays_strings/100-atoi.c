bb7c73073122d67d240875656f3eaac4a929ad69
falachabt <bennytenezeu@gmail.com>
2023-03-28 13:24:13 -0700

    yes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,39 @@
 #include <limits.h>
 
+/**
+ * _atoi - dlqsjflkdjqslkfjdqs
+ * @s: the input
+ *
+ * Return: the result of the tranformation
+ */
+
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
-    {
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-        {
-            result = INT_MAX;
-            break;
-        }
-        else if (result < INT_MIN / 10 || (result == INT_MIN / 10 && s[i] - '0' > -(INT_MIN % 10)))
-        {
-            result = INT_MIN;
-            break;
-        }
-        result = result * 10 + sign * (s[i] - '0');
-        i++;
-    }
-
-    return result;
+	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+		{
+			result = INT_MAX;
+			break;
+		}
+		else if (result < INT_MIN / 10 || (result == INT_MIN / 10 && s[i] - '0' > -(INT_MIN % 10)))
+		{
+			result = INT_MIN;
+			break;
+		}
+		result = result * 10 + sign * (s[i] - '0');
+		i++;
+	}
+	return (result);
 }
