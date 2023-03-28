f42513872a0987f3081e580f077045195405d8fd
falachabt <bennytenezeu@gmail.com>
2023-03-28 07:52:25 -0700

    modify rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,6 +15,7 @@ void rev_string(char *s)
 	int i;
 
 	while (s[len] != '\0')
+	{
 		len++;
 
 		for (i = 0; i < len / 2; i++)
@@ -23,5 +24,5 @@ void rev_string(char *s)
 			s[i] = s[len - i - 1];
 			s[len - i - 1] = temp;
 		}
-
+	}
 }
