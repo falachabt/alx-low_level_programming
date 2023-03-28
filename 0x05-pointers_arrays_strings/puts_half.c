5605700669b64bd69d205858f9cfd6a92f45a39b
falachabt <bennytenezeu@gmail.com>
2023-03-28 08:31:51 -0700

    update half

---
+++ 0x05-pointers_arrays_strings/puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print the second half of a string
+ * @str: the string to slice
+ *
+ * Return: nothing to return
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	start = ((len % 2 == 0) ? len / 2 : (len + 1) / 2) - 1;
+	for (start; i < len; start++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
