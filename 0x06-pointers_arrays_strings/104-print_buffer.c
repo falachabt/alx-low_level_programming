250ed8841eb70823ac0ec2c64679a69ebccf6620
falachabt <bennytenezeu@gmail.com>
2023-03-30 15:40:13 -0700

    first last

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,41 +8,40 @@
  */
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        for (j = i; j < i + 10; j++)
-        {
-            if (j < size)
-                printf("%02x", b[j]);
-            else
-                printf("  ");
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+				printf("%02x", b[j]);
+			else
+				printf("  ");
 
-            if (j % 2 == 1)
-                printf(" ");
-        }
+			if (j % 2 == 1)
+				printf(" ");
+		}
 
-        for (j = i; j < i + 10; j++)
-        {
-            if (j >= size)
-                break;
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+				break;
 
-            if (isprint(b[j]))
-                printf("%c", b[j]);
-            else
-                printf(".");
-        }
+			if (isprint(b[j]))
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
 
-        printf("\n");
-    }
+		printf("\n");
+	}
 }
-
