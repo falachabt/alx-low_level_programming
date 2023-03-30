4f02abe94039f5af9ff579a33eb3e144c89c4113
falachabt <bennytenezeu@gmail.com>
2023-03-30 14:50:13 -0700

    compre string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *);
+int _strcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
