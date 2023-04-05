9d5cd8f3ecc87f8e4f8c661de960d9d446ce2f46
falachabt <bennytenezeu@gmail.com>
2023-04-05 16:46:03 -0700

    6.1.0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@ int find_prime(int n, int i);
  *
  * Return: 1 if the number is prime 0 otherwise
  */
-int is_prime_number(int n);
+int is_prime_number(int n)
 {
 	if (n < 2)
 		return (0);
@@ -27,7 +27,7 @@ int find_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	if ((n % i) === 0)
+	if ((n % i) == 0)
 		return (0);
 	return (find_prime(n, i - 1));
 }
