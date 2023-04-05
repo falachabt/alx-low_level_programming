2360c453df622e5926b685d43d890366c6f9799a
falachabt <bennytenezeu@gmail.com>
2023-04-04 17:55:12 -0700

    yyui2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,13 +14,13 @@ char *_strchr(char *s, char c)
 
 	while (*s != '\0')
 	{
-		if(*s == c)
+		if (*s == c)
 			return (s);
 		s++;
 	}
 
 	if (c == '\0')
-	       return s;	
+		return (s);
 
 
 	return (NULL);
