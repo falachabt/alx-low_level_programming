1c1c7b79c9fdb33afae78b825d76101fd18bc971
falachabt <bennytenezeu@gmail.com>
2023-04-04 17:27:20 -0700

    yyy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - find a char in an string
+ * @s: the char in whic we fing a char
+ * @c: the char to fing
+ *
+ * Return: a pointer to the first occurence of c int
+ */
+
+char *_strchr(char *s; char c)
+{
+	int i = 0;
+	bool
+
+	while (s[i] != '\0')
+	{
+		if(s[i] == c)
+			return s[i]
+		i++;
+	}
+}
