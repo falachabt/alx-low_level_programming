32bf4e2905d70ed51578cf6f2b4c376d967aa38d
falachabt <bennytenezeu@gmail.com>
2023-04-20 01:41:15 -0700

    yes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that print a name 
+ * print_name - function that print a name
  *
  * @name: the name to be print
- * @f: the function 
+ * @f: the function
  *
- * Return: nothing to be return 
+ * Return: nothing to be return
  */
 void print_name(char *name, void (*f)(char *))
 {
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function on each element of an array
+ *
+ * @array: the array of element
+ * @size: the size of the array of element
+ * @action: the function to be executed on each element
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
