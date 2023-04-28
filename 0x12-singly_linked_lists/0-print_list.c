30a9c3c3edd7521b543b9dfcacf2825f125a3ce6
falachabt <bennytenezeu@gmail.com>
2023-04-28 01:40:06 -0700

    yes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_list - print a list a return the number of node
+ *
+ * @h: the list to run theow
+ *
+ * Return: the number of node 
+ */
+size_t print_list(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+			elements += 1;
+			continue;
+		}
+		printf("[%d] %s", h->len, h-str);
+		elements += 1;
+		h = h->next;
+	}
+
+	return elements;
+}
+
+
+
+
