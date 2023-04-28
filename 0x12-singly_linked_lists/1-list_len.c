c4cb640b0a2ebde8c1fc97598e19ccfe574c2252
falachabt <bennytenezeu@gmail.com>
2023-04-28 16:52:57 -0700

    null

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len * return the len of a list
+ *
+ * h: the list to give the length
+ *
+ * Return: the len of the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
