54573b5f0f7329db82cf8d78670a0bf2797a5684
falachabt <bennytenezeu@gmail.com>
2023-04-28 17:25:39 -0700

    yes

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,39 +9,39 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    unsigned int len = 0;
+	list_t *new_node, *last_node;
+	unsigned int len = 0;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    new_node->len = len;
-    new_node->next = NULL;
+	new_node->len = len;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        last_node = *head;
-        while (last_node->next != NULL)
-            last_node = last_node->next;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
 
-        last_node->next = new_node;
-    }
+		last_node->next = new_node;
+	}
 
-    return (new_node);
+	return (new_node);
 }
 
