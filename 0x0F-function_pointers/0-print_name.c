e286e03885a1905b68319af013f2bc52c4035dbc
falachabt <bennytenezeu@gmail.com>
2023-04-29 06:45:05 -0700

    yes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,6 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
-/**
- * print_name - function that print a name
- *
- * @name: the name to be print
- * @f: the function
- *
- * Return: nothing to be return
- */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
