4d2ef53774ba172199bb350a9042e8e79aaacf61
falachabt <bennytenezeu@gmail.com>
2023-04-29 06:37:31 -0700

    yes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct print - a structure that maps a format character to a print function
+ * @c: format character
+ * @f: print function
+ */
+typedef struct print
+{
+	char c;
+	void (*f)(va_list);
+} print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
