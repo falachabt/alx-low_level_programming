9305a7e3eefbad3f2b19e02399fd1a5fa7eb90ef
falachabt <bennytenezeu@gmail.com>
2023-04-29 08:28:31 -0700

    yes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,27 +1,68 @@
-import sys
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-def multiply(num1, num2):
-    if not num1.isdigit() or not num2.isdigit():
-        print("Error")
-        sys.exit(98)
+int is_valid_number(char *str);
+void multiply(char *num1, char *num2);
 
-    result = 0
-    for i in range(len(num2)):
-        digit = int(num2[-i-1])
-        temp = 0
-        for j in range(len(num1)):
-            temp += int(num1[-j-1]) * digit * (10**j)
-        result += temp * (10**i)
+int main(int argc, char **argv) {
+    if (argc != 3 || !is_valid_number(argv[1]) || !is_valid_number(argv[2])) {
+        printf("Error\n");
+        return 98;
+    }
+    
+    multiply(argv[1], argv[2]);
+    
+    return 0;
+}
 
-    print(result)
+int is_valid_number(char *str) {
+    if (!str) {
+        return 0;
+    }
+    
+    for (int i = 0; str[i]; i++) {
+        if (!isdigit(str[i])) {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Error")
-        sys.exit(98)
-
-    num1 = sys.argv[1]
-    num2 = sys.argv[2]
-
-    multiply(num1, num2)
+void multiply(char *num1, char *num2) {
+    int len1 = strlen(num1);
+    int len2 = strlen(num2);
+    int *result = calloc(len1 + len2, sizeof(int));
+    
+    for (int i = len1 - 1; i >= 0; i--) {
+        for (int j = len2 - 1; j >= 0; j--) {
+            int digit1 = num1[i] - '0';
+            int digit2 = num2[j] - '0';
+            int product = digit1 * digit2;
+            int pos1 = i + j;
+            int pos2 = i + j + 1;
+            int sum = product + result[pos2];
+            result[pos1] += sum / 10;
+            result[pos2] = sum % 10;
+        }
+    }
+    
+    int i = 0;
+    while (i < len1 + len2 && result[i] == 0) {
+        i++;
+    }
+    
+    if (i == len1 + len2) {
+        printf("0\n");
+    } else {
+        for (; i < len1 + len2; i++) {
+            printf("%d", result[i]);
+        }
+        printf("\n");
+    }
+    
+    free(result);
+}
 
