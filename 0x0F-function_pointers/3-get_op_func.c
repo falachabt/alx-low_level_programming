584aa80bce8fbe5af87b2488686128295e002625
falachabt <bennytenezeu@gmail.com>
2023-04-29 07:56:36 -0700

    est

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,29 +3,29 @@
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: operator passed as argument
- * Return: pointer to the function that corresponds to the operator given as a parameter
+ * Return: poinosponds to the operator given as a parameter
  */
 
 int (*get_op_func(char *s))(int, int)
 {
-  op_t ops[] = {
-      {"+", op_add},
-      {"-", op_sub},
-      {"*", op_mul},
-      {"/", op_div},
-      {"%", op_mod},
-      {NULL, NULL}
-  };
-  int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-  while (ops[i].op != NULL)
-  {
-    if (*s == *(ops[i].op) && *(s + 1) == '\0')
-      return (ops[i].f);
-    i++;
-  }
+	while (ops[i].op != NULL)
+	{
+		if (*s == *(ops[i].op) && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
-  printf("Error\n");
-  exit(99);
+	printf("Error\n");
+	exit(99);
 }
 
