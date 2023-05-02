d04318a11264e6879feba8a3fba8745a2b8160db
falachabt <bennytenezeu@gmail.com>
2023-05-02 14:49:52 -0700

    yes

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,33 +8,32 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow, *fast;
+	listint_t *slow, *fast;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    slow = head;
-    fast = head->next;
+	slow = head;
+	fast = head->next;
 
-    while (slow != NULL && fast != NULL && fast->next != NULL)
-    {
-        if (slow == fast) /* loop detected */
-        {
-            slow = head; /* reset slow to head */
-            fast = fast->next; /* advance fast by one */
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast) /* loop detected */{
+			slow = head; /* reset slow to head */
+			fast = fast->next; /* advance fast by one */
 
-            while (slow != fast) /* find the start of the loop */
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
+			while (slow != fast) /* find the start of the loop */
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
 
-            return (slow); /* return the address of the node */
-        }
+			return (slow); /* return the address of the node */
+		}
 
-        slow = slow->next; /* advance slow by one */
-        fast = fast->next->next; /* advance fast by two */
-    }
+		slow = slow->next; /* advance slow by one */
+		fast = fast->next->next; /* advance fast by two */
+	}
 
-    return (NULL); /* no loop found */
+	return (NULL); /* no loop found */
 }
