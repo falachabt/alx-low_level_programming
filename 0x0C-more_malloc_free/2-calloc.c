798132e2d8e544551dc762375c3096b4388265a6
falachabt <bennytenezeu@gmail.com>
2023-05-02 15:36:23 -0700

    yes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,30 @@
 #include <stdlib.h>
-
+/**
+ * _calloc -  fdsklqjlfkjqmf
+ * @nmemb: the fisth
+ * @size: the size
+ *
+ * Return: a pointer
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *ptr;
-  unsigned int i;
-  char *ptr_ch;
+	void *ptr;
+	unsigned int i;
+	char *ptr_ch;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-  ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * size);
 
-  if (ptr == NULL)
-    return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
-  ptr_ch = ptr;
+	ptr_ch = ptr;
 
-  for (i = 0; i < nmemb * size; i++)
-    ptr_ch[i] = 0;
+	for (i = 0; i < nmemb * size; i++)
+		ptr_ch[i] = 0;
 
-  return (ptr);
+	return (ptr);
 }
 
