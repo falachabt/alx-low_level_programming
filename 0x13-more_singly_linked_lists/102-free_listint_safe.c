2610119c7160b724b73270c77ed11d47ea37ce21
falachabt <bennytenezeu@gmail.com>
2023-05-02 14:39:53 -0700

    yes

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: pointer to the head of the list
- * Return: the size of the list that was freed
+ * free_listint_safe - Frees a listint_t list safely.
+ * @h: A pointer to a pointer to the head node of the list.
+ *
+ * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
     size_t count = 0;
-    listint_t *slow, *fast, *tmp;
+    listint_t *current, *next;
 
-    if (h == NULL || *h == NULL)
-        return (0);
+    if (!h || !(*h))
+        return (count);
 
-    slow = *h;
-    fast = (*h)->next;
-
-    while (slow != NULL)
+    current = *h;
+    while (current)
     {
+        next = current->next;
+        free(current);
         count++;
-        tmp = slow;
-        slow = slow->next;
-        free(tmp);
 
-        if (slow == fast) /* loop detected */
-            break;
+        /* Check if the next node has already been freed. */
+        if (next && (next < current))
+        {
+            /* Set the next pointer of the previous node to NULL. */
+            current->next = NULL;
+            *h = NULL;
+            exit(98);
+        }
 
-        if (fast != NULL && fast->next != NULL)
-            fast = fast->next->next;
-        else
-            fast = NULL;
+        current = next;
     }
 
     *h = NULL;
     return (count);
 }
+
