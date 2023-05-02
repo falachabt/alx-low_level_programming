fd65ef595032993ade0ed68bc55f386d2493a0dd
falachabt <bennytenezeu@gmail.com>
2023-05-02 13:58:05 -0700

    yes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 
 	while (current != NULL)
-    {
-        if (count == index)
-            return (current);
-        count++;
-        current = current->next;
-    }
-    return (NULL);
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
+	return (NULL);
 }
 
