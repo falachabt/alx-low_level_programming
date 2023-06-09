4a1545e341dece695e09a911916b8ced8ccf9dfd
falachabt <bennytenezeu@gmail.com>
2023-06-09 03:01:32 -0700

    remove static libraries

--- 0x09-static_libraries/2-strlen.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-int _strlen(char *s)
-{
-    int len = 0;
-    while (s[len] != '\0')
-        len++;
-    return (len);
-}
--- 0x09-static_libraries/6-abs.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int _abs(int n)
-{
-    if (n < 0)
-        return (n * -1);
-    else
-        return (n);
-}
